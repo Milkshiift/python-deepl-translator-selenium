a49981a34e2aae02c7bd47448f8cf32406ed0c80
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2024-06-17 22:16:50 +0300

    Use firefox instead of chromium

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ API-less somewhat fast DeepL translation using Selenium.
 1. Download translator.py and add it to your project
 2. Install selenium `pip install selenium` and webdriver-manager `pip install webdriver-manager`
 3. Call `await get_translation(text, langFrom, langTo)` in your code. You can find the language codes [here](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements)
-4. If you get an error that says chromium command not found, install Chromium from your Linux package manager. For Windows read selenium's documentation.
+4. If you get an error that says firefox command not found, install Firefox
--- translator.py
+++ translator.py
@@ -1,31 +1,19 @@
-from selenium import webdriver 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.chrome.service import Service as ChromeService 
-from webdriver_manager.chrome import ChromeDriverManager 
-from webdriver_manager.core.os_manager import ChromeType
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-options = webdriver.ChromeOptions() 
-options.add_argument('--headless') # Remove this to see the browser window
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-# These are maybe useless, but hopefully not
-options.add_argument('--disable-software-rasterizer')
-options.add_argument('--disable-extensions')
-options.add_argument('--disable-background-networking')
-options.add_argument('--disable-default-apps')
-options.add_argument('--disable-sync')
-options.add_argument('--disable-translate')
-options.add_argument('--metrics-recording-only')
-options.add_argument('--no-first-run')
+options = webdriver.FirefoxOptions()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options) 
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
 url = "https://www.deepl.com/translator"
 driver.get(url)
 
-async def get_translation(text, langFrom, langTo):
+def get_translation(text, langFrom, langTo):
     driver.get(f"https://www.deepl.com/translator#{langFrom}/{langTo}/")
     
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-labelledby='translation-source-heading']")))
@@ -40,3 +28,8 @@ async def get_translation(text, langFrom, langTo):
     translation = target_element.text
 
     return translation
+
+print(get_translation("but daddy 🥺 👉 👈", "ru", "en"))
+
+# Close the browser window
+driver.quit()
